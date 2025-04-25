12f06b6245a28a8ebc64e5051e6cdefa8441132c
Muneeb912-cmd <muneebe914@gmail.com>
2025-04-25 18:13:30 +0500

    Domain: Implemented app Domain layer

--- app/src/main/java/com/example/ble_data_synchronizer/domain/usecases/SynchronizedDataUseCases.kt
+++ app/src/main/java/com/example/ble_data_synchronizer/domain/usecases/SynchronizedDataUseCases.kt
@@ -1,2 +1,31 @@
 package com.example.ble_data_synchronizer.domain.usecases
 
+import com.example.ble_data_synchronizer.data.model.DataChunk
+import com.example.ble_data_synchronizer.data.repository.DataSynchronizerRepository
+import com.example.ble_data_synchronizer.utils.ConnectionStatus
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SynchronizeDataUseCase @Inject constructor(
+    private val repository: DataSynchronizerRepository
+) {
+    suspend fun storeDataChunk(dataChunk: DataChunk) {
+        repository.storeDataChunk(dataChunk)
+    }
+
+    suspend fun synchronizeData() {
+        repository.synchronizePendingData()
+    }
+
+    fun getConnectionStatus(): Flow<ConnectionStatus> {
+        return repository.connectionStatus
+    }
+
+    fun getPendingUploadsCount(): Flow<Int> {
+        return repository.pendingUploadsCount
+    }
+
+    fun getAllDataChunks(): Flow<List<DataChunk>> {
+        return repository.allDataChunks
+    }
+}
